b58230365be44869a13ecab62294d338cb328685
ldrldr88 <8399683@gmail.com>
2026-06-11 09:54:45 +0800

    add google tag

--- build.js
+++ build.js
@@ -49,6 +49,16 @@ function legacyRedirectHtml() {
   return `<!doctype html>
 <html lang="zh-Hans">
 <head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-JHQMK2RV1D"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-JHQMK2RV1D');
+  </script>
+
   <meta charset="utf-8">
   <title>页面已迁移</title>
   <meta name="robots" content="noindex, follow">
